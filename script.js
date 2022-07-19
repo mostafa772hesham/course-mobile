298d3a4933ba8c9035a0e51cffae1ec9c43d6761
mostafa772hesham <85575020+mostafa772hesham@users.noreply.github.com>
2022-07-19 23:05:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function CCNA210(){
+ var myFrame=   
+//  myFrame.src ="https://www.youtube.com/embed/videoseries?controls=0&amp;list=PLped9VG7STA_pfWX_VMbSnwMd3UNA2oFU&ab_channel=AbeerHosni?rel=0"
+console.log(myFrame);
+}
